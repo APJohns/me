44434f8ee16f97d20e7122f1cc89333419c523ba
Ash Johns <mail@ashpjohns.com>
2025-04-09 00:06:40 -0400

    break projects into work and projects

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,22 +1,34 @@
-// 1. Import utilities from `astro:content`
 import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
-// 2. Define your collection(s)
+import { defineCollection, z, type ImageFunction } from 'astro:content';
+
+const sharedDetails = (image: ImageFunction) => ({
+  title: z.string(),
+  description: z.string(),
+  company: z.string().optional(),
+  techStack: z.array(z.string()),
+  links: z.array(z.array(z.string())),
+  thumbnail: image(),
+  screenshot: image(),
+  sortOrder: z.number(),
+  draft: z.boolean().optional(),
+});
+
+const work = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.md', base: './src/content/work' }),
+  schema: ({ image }) =>
+    z.object({
+      ...sharedDetails(image),
+    }),
+});
+
 const projects = defineCollection({
   loader: glob({ pattern: '**/[^_]*.md', base: './src/content/projects' }),
   schema: ({ image }) =>
     z.object({
-      title: z.string(),
-      description: z.string(),
-      company: z.string().optional(),
-      techStack: z.array(z.string()),
-      links: z.array(z.array(z.string())),
-      thumbnail: image(),
-      screenshot: image(),
-      sortOrder: z.number(),
-      draft: z.boolean().optional(),
+      ...sharedDetails(image),
     }),
 });
-// 3. Export a single `collections` object to register your collection(s)
-//    This key should match your collection directory name in "src/content"
-export const collections = { projects };
+
+//  Export a single `collections` object to register your collection(s)
+//  This key should match your collection directory name in "src/content"
+export const collections = { projects, work };
